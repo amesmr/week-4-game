461861cdebe5b9067c2ee236143652de84e1fe74
Matthew Ames <Matthew Ames>
2016-12-15 00:15:12 -0500

    correcting some glaring logic errors generating random numbers.  Fiddling with layout.

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,24 +6,30 @@ var jewelImgs = ["assets/images/bca1d0ea2e9bf6cf46adf13390b887b0.jpg",
     "assets/images/birth-stone.jpg",
     "assets/images/emerald-08.jpg"
 ];
-var imgPicVal = [-1, -1, -1, -1];
+var imgPicVal = [];
 
 function initGame() {
     var imgNumVal = 0;
+    imgPicVal = [];
+
+    //fill imgPicVal arry with unique numbers btween 1 & 12
+    while (imgPicVal.length < 4) {
+        tmp = Math.floor((Math.random() * (12)) + 1);
+        if (imgPicVal.indexOf(tmp) == -1) {
+            imgPicVal.push(tmp);
+        }
+    }
+    // console.log("imgPicVal = " + imgPicVal);
 
     // generate a random number to help us randomly load the picture elements
     var picJwlRand = Math.floor(Math.random() * jewelImgs.length);
-    var picJwlRandVal = Math.floor((Math.random() * (12)) + 1);
-
     for (i = 0; i < jewelImgs.length; i++) {
         // use picRand to display the images randomly
         // using modulus operator will allow us to pick them uniquely
         $("#jewel" + i).attr("src", jewelImgs[(jewelImgs.length + picJwlRand + i) % jewelImgs.length]);
-        // let's go ahead and get a unique number for each jewel
-        imgPicVal[i] = (picJwlRandVal + 12 + i) % 12;
     }
     // generate a number btwn 19 and 120 (inclusive) and put it into the RandomNumber element
-    $("#RandomNumber").html(Math.floor(Math.random() * (((120 - 19 + 1) % 19) + 19)));
+    $("#RandomNumber").html(Math.floor(Math.random() * ((120 - 19 + 1))) + 19);
     // reset the player's score to 0
     $("#totalScore").html("0");
 
@@ -40,7 +46,7 @@ function handleClick(obj) {
         }
     }
     var newTotal = parseInt(imgVal) + parseInt(currTotal);
-    console.log("newTotal = " + newTotal);
+    // console.log("newTotal = " + newTotal);
     if (newTotal > currRandNum) {
         // you lose
         // update losses and call init
@@ -76,35 +82,12 @@ $(document).ready(function() {
     $("#jewel3").on("click", handleClick);
     ReSize();
 
-    function ReSize() {
-        if ($(this).width() <= '480') {
-            $(".pic").css({
-                "width": "50px",
-                "height": "50px",
-            });
-        } else if ($(this).width() <= '768') {
-            $(".pic").css({
-                "width": "80px",
-                "height": "80px",
-            });
-        } else if ($(this).width() <= '980') {
-            $(".pic").css({
-                "width": "100px",
-                "height": "100px"
-            });
-        } else {
-            $(".pic").css({
-                "width": "120px",
-                "height": "120px"
-            });
-        }
-    }
     // let's annoy the crap out of the player so that he can't think straight
     var objs = $('.opacity');
 
     function runIt() {
         objs.animate({ opacity: '1' }, 1000);
-        objs.animate({ opacity: '0.75' }, 1000, runIt);
+        objs.animate({ opacity: '0.7' }, 1000, runIt);
     }
     runIt();
 
@@ -118,3 +101,27 @@ $(document).ready(function() {
     makeIt();
 
 });
+
+function ReSize() {
+    if ($(this).width() <= '480') {
+        $(".pic").css({
+            "width": "50px",
+            "height": "50px",
+        });
+    } else if ($(this).width() <= '768') {
+        $(".pic").css({
+            "width": "80px",
+            "height": "80px",
+        });
+    } else if ($(this).width() <= '980') {
+        $(".pic").css({
+            "width": "100px",
+            "height": "100px"
+        });
+    } else {
+        $(".pic").css({
+            "width": "120px",
+            "height": "120px"
+        });
+    }
+}
